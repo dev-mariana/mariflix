0942bbd2ac235c1e632a1d5f1806e40325dd1d29
Mariana Bastos Rodrigues <you@example.com>
2020-07-28 12:15:17 -0300

    footer atualizado

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { FooterBase } from './styles';
+import logo from '../../assets/images/logo.png';
 
 function Footer() {
   return (
     <FooterBase>
-      <a href="https://www.alura.com.br/">
-        <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
-      </a>
+      {/* <a href="https://www.alura.com.br/"> */}
+        <img className="Logo" src={logo} alt="Logo Mariflix"/>
+      {/* </a> */}
       <p>
         Orgulhosamente criado durante a
         {' '}
